2d1a5e7699bebffe255a87522308dd9cf0b18426
Ketan Mahandule <56297133+lucifer007-coder@users.noreply.github.com>
2025-08-20 08:49:23 +0000

    Update stats_utils.py

--- utils/stats_utils.py
+++ utils/stats_utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from typing import Dict, Any, Optional
 import streamlit as st
-from .data_utils import dtype_split
+from data_utils import dtype_split
 
 logger = logging.getLogger(__name__)
 
